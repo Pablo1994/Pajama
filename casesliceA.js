a225104fafc73459187844bafa55d69fd16338a7
jotaporras <jporras06@gmail.com>
2014-10-13 18:50:37 -0600

    Estos eran los casos de prueba que use

---
+++ casesliceA.js
@@ -0,0 +1,18 @@
+load('rt/util.js');
+
+function casesliceA(a) {
+  return (function($x) {
+    return (function($n, $c) {
+      if ((function($x) {
+        return (patListTest)([any], $x.slice(0, 1)) && (function($x) {
+          return (patListTest)([function($x) {
+            return ($x[0] === 1);
+          }], $x);
+        })($x.slice(1));
+      })($n)) return (function($x) {
+        return $x[0];
+      })($n);
+      else return ($c)($n);
+    })($x, fail);
+  })(a);
+}
---
+++ casesliceC.js
@@ -0,0 +1,22 @@
+load('rt/util.js');
+
+function casesliceCB(a) {
+  return (function($x) {
+    return (function($n, $c) {
+      if ((function($x) {
+        return (patListTest)([function($x) {
+          return ($x[0] === 2);
+        }, function($x) {
+          return (patListTest)([any], $x[1].slice(0, 1)) && (function($x) {
+            return (patListTest)([function($x) {
+              return ($x[0] === 2);
+            }], $x);
+          })($x[1].slice(1));
+        }], $x);
+      })($n)) return (function($x) {
+        return $x[1][0];
+      })($n);
+      else return ($c)($n);
+    })($x, fail);
+  })(a);
+}
