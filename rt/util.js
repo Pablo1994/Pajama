b5d030630394032bfd84e1be55513586e71ec300
Pablo Arias <jpaz199448@gmail.com>
2014-10-22 20:28:37 -0600

    caso10 funcionando hasta que el profe me conteste si está bien lo que hice o no

--- rt/util.js
+++ rt/util.js
@@ -52,9 +52,9 @@ function expectFAIL(expr, f) {
     return false;
 }
 
-function unshift(a, x) {
-    a.unshift(x);
-    return a;
+function unshift(a) {
+    print(a[0].unshift(a[1]));
+    return a[0];
 }
 
 function identidad(x) x;
